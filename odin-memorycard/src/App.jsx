7e9b0ffc7eab80f41a4182ef1d865520ffc4bcdd
Marucha Johnstone <marucha.webdev@gmail.com>
2024-05-10 21:43:53 +0300

    feat(components): Clean up App component, styled abit

--- odin-memorycard/src/App.jsx
+++ odin-memorycard/src/App.jsx
@@ -1,51 +1,23 @@
-// lib imports
-import { useState } from 'react'
-import {v4 as uuid} from 'uuid';
-// local imports
-import { Cards } from './Cards';
-import Header from './Header';
-// assets
-import './App.css'
+// components
+import { MainDisplay } from "./Components/MainDisplay"
 
-const pokemonList = [
-  {name:'gengar',id: uuid()},
-  {name:'ditto',id: uuid()},
-  {name:'pikachu',id: uuid()},
-  {name:'pidgey',id: uuid()},
-  {name:'roseia',id: uuid()},
-  {name:'squirtle',id: uuid()},
-  {name:'zapdos',id: uuid()}
-]
+// Styled container 
+function Container({ children }) {
+  return (
+    <div className="md:container m-auto p-4">
+        {children}
+    </div>
+  )
+}
+
 
 export default function App() {
-  const[currentScore,setCurrentScore] = useState(0);
-  const[highscore, setHighScore] = useState(0);
-  const[pokeCards,setPokeCards] = useState(pokemonList);
-  const[clickedId, setClickedId] = useState([]);
-
-  //shuffles and updates score.
-  const handleClick = (id) => {
-    if(clickedId.includes(id)) {
-      if (currentScore > highscore) {
-          setHighScore(currentScore);
-        }
-          setCurrentScore(0);
-          setClickedId([]);
-        } else {
-          // eslint-disable-next-line no-const-assign
-          setCurrentScore(currentScore + 1);
-          setClickedId([...clickedId, id]);
-
-        }
-      const shuffleCards = [...pokeCards].sort(()=> Math.random() - 0.5);
-      setPokeCards(shuffleCards);
-  }
   return(
-    <section>
-      <Header currentscore={currentScore} highScore={highscore} />
-      <Cards cards={pokeCards} onClick={handleClick} />
-    </section>
+    <div className='app'>
+      <Container>
+        <MainDisplay />
+      </Container>
+    </div>
   )
-
 }
 
