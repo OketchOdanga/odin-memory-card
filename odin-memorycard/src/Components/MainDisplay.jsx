78718942df0208e500d4d65d9fbda813e5a47ed4
Marucha Johnstone <marucha.webdev@gmail.com>
2024-05-10 21:44:54 +0300

    chore(components): Proposed components

---
+++ odin-memorycard/src/Components/MainDisplay.jsx
@@ -0,0 +1,10 @@
+
+
+export function MainDisplay() {
+  return (
+    <div className="">
+      <h2>All Application Data is nested below this</h2>
+      <h3>Manage state from this component</h3>
+    </div>
+  )
+}
