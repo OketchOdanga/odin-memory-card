03287b13c7d0ad0977dfb4fccbb00b37fbaa0993
Marucha Johnstone <marucha.webdev@gmail.com>
2024-05-11 23:03:37 +0300

    feat(components): Pokemon display component, uses poke url passed down from initial app fetch

---
+++ odin-memorycard/src/Components/Pokemon.jsx
@@ -0,0 +1,59 @@
+// lib imports
+import { useState, useEffect } from "react"
+import { v4 as uuid } from "uuid"
+
+// import { defaultPoke } from './MockData'
+
+export const CardList = ({ pokes, clickAction }) => {
+  return (
+    <div className="space-y-2">
+      <h4>Pokemon </h4>
+      <section className="cardlist">
+        {
+          pokes.map((poke) => <PokeCard key={uuid()} poke={poke} clickAction={clickAction}/>)
+        }
+      </section>
+    </div>
+  )
+}
+
+const PokeCard = ({ poke, clickAction }) => {
+  const { name, url } = poke
+  const [ pokeInfo, setPokeInfo ] = useState({})
+  const [ loading, setLoading ] = useState(true)
+
+  useEffect(() => {
+    fetch(url)
+    .then(r => r.json())
+    .then(d => {
+      setPokeInfo(d)
+      setLoading(false)
+    })
+    .catch(e => console.error(e))
+  },[url])
+
+  // useEffect(() => {
+  //   const dataTimeout = setTimeout(() => {
+  //     setLoading(false)
+  //     setPokeInfo(defaultPoke)
+  //   },500)
+
+  //   return () => clearTimeout(dataTimeout)
+  // })
+
+  return (
+    <div>
+      {
+        loading ?
+          <section className="loader">
+            <h5> Loading ... </h5>
+          </section>
+        :
+        <section className="card" onClick={() => clickAction(pokeInfo.id)}>
+          <img src={pokeInfo.sprites.front_default} className=""  alt="pokemon image"/>
+          <h5>{name}</h5>
+        </section>
+      }
+    </div>
+  )
+}
