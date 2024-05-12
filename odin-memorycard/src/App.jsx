646882053be2daef5579a9f78e46100981c788be
Austine Odanga <austinoketches@gmail.com>
2024-05-12 12:42:10 +0300

    refactor

--- odin-memorycard/src/App.jsx
+++ odin-memorycard/src/App.jsx
@@ -2,45 +2,54 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Header';
 import { Cards } from './Cards';
-import {v4 as uuid} from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 
 const pokemonList = [
-  {name:'gengar',id: uuid()},
-  {name:'ditto',id: uuid()},
-  {name:'pikachu',id: uuid()},
-  {name:'pidgey',id: uuid()},
-  {name:'roseia',id: uuid()},
-  {name:'squirtle',id: uuid()},
-  {name:'zapdos',id: uuid()}
+  {name:'gengar',id: uuidv4()},
+  {name:'ditto',id: uuidv4()},
+  {name:'pikachu',id: uuidv4()},
+  {name:'pidgey',id: uuidv4()},
+  {name:'roselia',id: uuidv4()},
+  {name:'squirtle',id: uuidv4()},
+  {name:'zapdos',id: uuidv4()}
 ]
 
 export default function App() {
   const[currentScore,setCurrentScore] = useState(0);
-  const[highscore, setHighScore] = useState(0);
+  const[highScore, setHighScore] = useState(0);
   const[pokeCards,setPokeCards] = useState(pokemonList);
   const[clickedId, setClickedId] = useState([]);
 
   //shuffles and updates score. 
   const handleClick = (id) => {
-    if(clickedId.includes(id)) {
-      if (currentScore > highscore) {
-          setHighScore(currentScore);
-        }
-          setCurrentScore(0);
-          setClickedId([]);
-        } else {
-          // eslint-disable-next-line no-const-assign
-          setCurrentScore(currentScore + 1);
-          setClickedId([...clickedId, id]);
-          
-        }
-      const shuffleCards = [...pokeCards].sort(()=> Math.random() - 0.5);
-      setPokeCards(shuffleCards);
+    if (clickedId.includes(id)) {
+      if (currentScore > highScore) {
+        setHighScore(currentScore);
+      }
+      setCurrentScore(0);
+      setClickedId([]);
+      
+    } else {
+      // eslint-disable-next-line no-const-assign
+      setCurrentScore(currentScore + 1);
+      setClickedId([...clickedId, id]);
+      
+    }
+    const shuffleCards = [...pokeCards].sort(()=> Math.random() - 0.5);
+    setPokeCards(shuffleCards);     
   }
+
   return(
     <section>
-      <Header currentscore={currentScore} highScore={highscore} />
-      <Cards cards={pokeCards} onClick={handleClick} />
+      <h2>POKEMON</h2>
+      <Header currentscore={currentScore} highscore={highScore} />
+      <section>
+        {pokeCards.map((card) => (
+          <Cards key={card.id} pokemon={card.name}  cards={pokeCards} onClick={handleClick}/>
+        ))}
+      </section>
+      
+      
     </section>
   )
 
--- odin-memorycard/src/Header.jsx
+++ odin-memorycard/src/Header.jsx
@@ -1,11 +1,11 @@
 
 // displays the scoreboard.
 // eslint-disable-next-line react/prop-types
-export default function Header({currentscore,highScore}) {
+export default function Header({currentscore,highscore}) {
     return (
-       <section>
+       <section className="header">
           <div className="current-score">Current Score: {currentscore}</div>
-          <div className="highscore">Highscore: {highScore}</div>
+          <div className="highscore">Highscore: {highscore}</div>
         </section>
     )
 }
